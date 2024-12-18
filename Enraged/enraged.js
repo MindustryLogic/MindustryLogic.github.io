96904f7dd829eb08ba393d087a91087b4fda8c9d
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2024-12-18 06:43:07 +0000

    since this is a mv this really should not be that messy

--- Enraged/enraged.js
+++ Enraged/enraged.js
@@ -1,13 +1,17 @@
 //really should nuke it
 //nuked time to rebuild
+let n = 0
+function loop_num(){
+    n+=1
+}
 function random_test(){
     var num = Math.random()
     console.log(num)
     document.querySelectorAll('.random_test').forEach(el => {
         el.style.setProperty('--ran-num', num);
+        el.style.setProperty('--loop-count',n);
     })
 }
-let n = 0
 function loop_set_func(a,b,c){
     if (n < c) {
     setInterval(a,b)
@@ -18,3 +22,13 @@ function loop_set_func(a,b,c){
         return(null)
     }
 }
+let count = 0;
+const timer = setInterval(() => {
+    count++;
+    console.log(count * 50 + 'ms');
+}, 50);
+
+// Optional: Stop timer after certain duration
+setTimeout(() => {
+    clearInterval(timer);
+}, 5000); // Stops after 5 seconds
