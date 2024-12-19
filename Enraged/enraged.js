6ba04bf19688e10746a24ae4978b7a49fb0ae26d
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2024-12-19 03:22:15 +0000

    its working. its also not working

--- Enraged/enraged.js
+++ Enraged/enraged.js
@@ -7,12 +7,20 @@ const loop_count = setInterval(() => {
 //well that works
 //time to rewrite loop iteration count thing
 //const loop_set_func = setInterval(temp_sep,50,random_test,1000,10);
-const the_2nd = setInterval(random_test,1000);
+const the_2nd = setInterval(startTitleShake,50);
 setTimeout(() => {
     clearInterval(the_2nd);
 }, 10000);
 //this works idk how
-    
+let fadeIn = 0
+function startTitleShake(){
+    var vert = Math.round((40*Math.random())-20);
+    fadeIn += 1;
+    document.querySelectorAll('.start').forEach(el => {
+        el.style.setProperty('--vert-shake',vert);
+        el.style.setProperty('--opac',fadeIn);
+    })
+}
 function random_test(){
     var num = Math.random()
     console.log(num)
