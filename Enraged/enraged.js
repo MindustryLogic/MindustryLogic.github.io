ebe1af5b6887cd8da69dc8cd8a258e07d9b54ff6
MindustryLogic <xyndermultilanguagelism@gmail.com>
2024-12-23 17:46:43 +0900

    o welp it syncs itself

--- Enraged/enraged.js
+++ Enraged/enraged.js
@@ -1,51 +1,39 @@
-//really should nuke it
-//nuked time to rebuild
-const play = false
-let n = 0;
+//reformat
+let begin = false;
+let sleep_done = false;
 function beginTheChaos(){
-play = true;
-}//should maybe rethink this
-const loop_count = setInterval(() => {
-    n++;
-},1000);
-var RGC = new Audio('RGC.mp3');
-RGC.play();
-//well that works
-//time to rewrite loop iteration count thing
-//const loop_set_func = setInterval(temp_sep,50,random_test,1000,10);
-const the_2nd = setInterval(startTitleShake,50);
-setTimeout(() => {
-    clearInterval(the_2nd);
-}, 10000);
-//this works idk how
-let fadeIn = 0
+    begin = true;
+    beginChaos();
+}
 function startTitleShake(){
     var vert = Math.round((40*Math.random())-20);
-    fadeIn += 1;
     document.querySelectorAll('.start').forEach(el => {
         el.style.setProperty('--vert-shake',vert);
-        //el.style.setProperty('--opac',fadeIn);
     })
 }
-function random_test(){
-    var num = Math.random()
-    console.log(num)
-    document.querySelectorAll('.random_test').forEach(el => {
-        el.style.setProperty('--ran-num', num);
-        el.style.setProperty('--loop-count',n);
-    })
+function blank(){
+    sleep_done = true;
+    console.log(sleep_done);
 }
-let count = 0;
-let count_real = 0;
-const timer = setInterval(() => {
-    count++;
-    count_real = count * 50
-    if ((count_real % 1000) === 0 ){
-        console.log(count_real + "ms")
-    }
-}, 50);
+function beginChaos(){
+    if (begin == true){
+        setTimeout(blank,3000);
+        if (sleep_done == true){
+        console.log("hi");
+        var bgMusic = new Audio('RGC.mp3');
+        var music = document.querySelector("audio");
+        music.addEventListener('play',console.log("it plays"));
+        bgMusic.play();
+        const theShake = setInterval(startTitleShake,50);
+        setTimeout(()=>{
+            clearInterval(theShake);
+        },10000);
+        document.querySelectorAll(":root").forEach(el => {
+            el.style.setProperty('--main-anim-ctrl', "running");
+        });
+    } else {
+        setTimeout(beginChaos,3000);
+    }}
+}
 
-// Optional: Stop timer after certain duration
-/*setTimeout(() => {
-    clearInterval(timer);
-}, 5000); // Stops after 5 seconds */
+
