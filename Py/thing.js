dc450655ca21134ab5e344030a99c364ccb36d04
MindustryLogic <xyndermultilanguagelism@gmail.com>
2024-12-23 13:56:19 +0900

    now ti display 2 resolution

--- Py/thing.js
+++ Py/thing.js
@@ -1,5 +1,8 @@
 function getResolution(){
+    const windowW = window.innerWidth;
+    const windowH = window.innerHeight;
     const screenW = window.screen.width;
     const screenH = window.screen.height;
-    document.getElementById("Resolution").innerHTML = screenH + "px in height, " + screenW + "px in width";
+    document.getElementById("Resolution").innerHTML = windowH + "px in height, " + windowW + "px in width" + " of window space";
+    document.getElementById("outer_res").innerHTML = screenH + "px in height, " + screenW + "px in width" + " of screen space";
 }
