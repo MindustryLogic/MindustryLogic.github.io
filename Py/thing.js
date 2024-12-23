be7e5fe89347f8f503cf6be97a6eb0161980a828
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2024-12-23 06:50:00 +0000

    uh

--- Py/thing.js
+++ Py/thing.js
@@ -4,5 +4,5 @@ function getResolution(){
     const screenW = window.screen.width;
     const screenH = window.screen.height;
     document.getElementById("Resolution").innerHTML = windowH + "px in height, " + windowW + "px in width" + " of window space";
-    document.getElementById("outer_res").innerHTML = screenH + "px in height, " + screenW + "px in width" + " of screen space";
+    document.getElementById("outer_res").innerHTML = screenH + "px in height, " + screenW + "px in width" + " of screen space." + "This does not account for orientation";
 }
