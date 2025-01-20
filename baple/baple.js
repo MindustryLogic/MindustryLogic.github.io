30b5993df01623414033436f89158f0636679f10
MindustryLogic <xyndermultilanguagelism@gmail.com>
2025-01-20 14:05:39 +0900

    why am i doing this

--- baple/baple.js
+++ baple/baple.js
@@ -2,12 +2,16 @@ const broWidth = window.innerWidth;
 const broHeight = window.innerHeight;
 let AdjRatio = broWidth / (16 / 9)
 const showWindowRes = document.getElementById("brores").innerHTML = broWidth + "width" + broHeight + "height" + AdjRatio + "16:9"
-function HTMLconsole(arg){
-    console.log(arg);
-    document.getElementById("HTMLconsole")
+const fixRatio = document.querySelectorAll(":root").forEach(cssVar => {
+    cssVar.style.setProperty("--window-height", broHeight);
+    cssVar.style.setProperty("--window-width", broWidth);
+});/* ya idk why am i doing this but anyway*/
+function HTMLconsole(str){
+    console.log(str);
+    document.getElementById("HTMLconsole").innerHTML = str;
 }
 function theHTMLconsoleShowThing(){
-    if (document.querySelector(":root").getAttribute("--console-open") === "none") {
+    if (window.getComputedStyle(document.querySelector(":root")).getPropertyValue("--console-open") === "none") {
         document.querySelector(":root").style.setProperty("--console-open", "table");
         HTMLconsole("it open");
     }else{
