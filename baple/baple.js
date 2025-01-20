990d82e2ec19f4c323f7d1df3fefa3ba09629fef
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2025-01-20 05:48:28 +0000

    yeetus

--- baple/baple.js
+++ baple/baple.js
@@ -3,8 +3,8 @@ const broHeight = window.innerHeight;
 let AdjRatio = broWidth / (16 / 9)
 const showWindowRes = document.getElementById("brores").innerHTML = broWidth + "width" + broHeight + "height" + AdjRatio + "16:9"
 const fixRatio = document.querySelectorAll(":root").forEach(cssVar => {
-    cssVar.style.setProperty("--window-height", broHeight);
-    cssVar.style.setProperty("--window-width", broWidth);
+    cssVar.style.setProperty("--window-height", (broWidth * 9/16) + "px");
+    cssVar.style.setProperty("--window-width", broWidth + "px");
 });/* ya idk why am i doing this but anyway*/
 function HTMLconsole(str){
     console.log(str);
@@ -14,6 +14,7 @@ function theHTMLconsoleShowThing(){
     if (window.getComputedStyle(document.querySelector(":root")).getPropertyValue("--console-open") === "none") {
         document.querySelector(":root").style.setProperty("--console-open", "table");
         HTMLconsole("it open");
+        document.getElementById("measure").style.height = AdjRatio + "px";
     }else{
         document.querySelector(":root").style.setProperty("--console-open", "none");
     }
