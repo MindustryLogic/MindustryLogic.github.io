aca9a0a14fe1989a5d49bc2c64b8b66f65a6235f
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2025-01-20 00:38:09 +0000

    what again

--- baple/baple.js
+++ baple/baple.js
@@ -1,1 +1,16 @@
 const broWidth = window.innerWidth;
+const broHeight = window.innerHeight;
+let AdjRatio = broWidth / (16 / 9)
+const showWindowRes = document.getElementById("brores").innerHTML = broWidth + "width" + broHeight + "height" + AdjRatio + "16:9"
+function HTMLconsole(arg){
+    console.log(arg);
+    document.getElementById("HTMLconsole")
+}
+function theHTMLconsoleShowThing(){
+    if (document.querySelector(":root").getAttribute("--console-open") === "none") {
+        document.querySelector(":root").style.setProperty("--console-open", "table");
+        HTMLconsole("it open");
+    }else{
+        document.querySelector(":root").style.setProperty("--console-open", "none");
+    }
+}
