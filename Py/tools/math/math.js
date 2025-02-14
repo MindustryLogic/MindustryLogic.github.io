a4dd14f6b8db1a76d6b7adc1c5922ff9e76eded3
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2025-02-14 03:39:40 +0000

    something work and something doesn't.

--- Py/tools/math/math.js
+++ Py/tools/math/math.js
@@ -7,8 +7,6 @@ async function CALLTHEJSON(){
     const wordArrays = Object.values(grabbedWords);
     fromJSON = wordArrays[mode];
     HTMLconsole(fromJSON);
-    fromJSON = wordArrays["modes"];
-    HTMLconsole(fromJSON);
 }
 CALLTHEJSON();
 function HTMLconsole(str){
@@ -23,11 +21,9 @@ function HTMLconsole(str){
 const modeSwitchButtons = document.querySelectorAll("button#modeSwitch");
 const subModeButtons = document.querySelectorAll("div#unit-conv button");
 const modeDisplay = document.querySelector("div#mode-display p");
-const Celsius = document.getElementById("Celsius");
-const Kelvin = document.getElementById("Kelvin");
-const Fahrenheit = document.getElementById("Fahrenheit");
-const Rankine = document.getElementById("Rankine");
-const Romer = document.getElementById("Romer");
+const ConvPlace = document.getElementById("innerContent");
+let HowManyStuffInConv = 5;
+let CalcForWidthOfConvThingCusInputHasMargin = 19.5;
 window.addEventListener('DOMContentLoaded', (event) =>{
     event.preventDefault();
     modeSwitchButtons.forEach(button => button.addEventListener("click", function () {
@@ -35,54 +31,83 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         console.log(mode + "this is mode num");
         console.log(submode + "this is submode num");
         console.log(fromJSON);
-        setTimeout(() => { for (let i = 0; i < fromJSON.length; i++) { subModeButtons[i].textContent = fromJSON[i]; } }, 100);//This thingy here need to click 2 times probably due to getting the words from a JSON is async and it does not sync ig
+        setTimeout(() => { for (let i = 0; i < fromJSON.length; i++) { subModeButtons[i].textContent = fromJSON[i]; } }, 250);//This thingy here need to click 2 times probably due to getting the words from a JSON is async and it does not sync ig
+        switch (mode) {
+            case 0:
+                modeDisplay.textContent = "Unit";
+                break;
+            case 1:
+                modeDisplay.textContent = "Thing";
+                break;
+        }
     }));
     subModeButtons.forEach(button => button.addEventListener("click", function () {
-        
+        if (mode === 0) {
+            switch (submode) {
+                case 0:
+                    ConvPlace.innerHTML = TemperatureConvThing();
+                    HowManyStuffInConv = document.querySelectorAll("#innerContent input").length;
+                    CalcForWidthOfConvThingCusInputHasMargin = (100 / HowManyStuffInConv) - HowManyStuffInConv * 0.1;
+                    document.querySelectorAll("#innerContent input label").style.setProperty("width", CalcForWidthOfConvThingCusInputHasMargin + "%");
+                    break;
+                case 1:
+                    ConvPlace.innerHTML = LengthConvThing();
+                    HowManyStuffInConv = document.querySelectorAll("#innerContent input").length;//seems like it isn't working
+                    CalcForWidthOfConvThingCusInputHasMargin = (100 / HowManyStuffInConv) - HowManyStuffInConv * 0.1;
+                    document.querySelectorAll("#innerContent input label").style.setProperty("width", CalcForWidthOfConvThingCusInputHasMargin + "%");
+                    break;
+            }
+        }
     }))
 })
 //TODO:Find out how to swap the html
 //or should i just like hide it
 // Put on hold i can't think again
-const temperatureInputs = document.getElementById('innerContent');
+const ConvInput = document.getElementById('innerContent');
 
-temperatureInputs.addEventListener('keydown', function(event) {
+ConvInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
         const targetId = event.target.id;
         const whatKey = Number(event.target.value);
-        switch (targetId) {
-            case 'Celsius':
-                Kelvin.value = whatKey + 273.15;
-                Fahrenheit.value = (whatKey * 9/5) + 32;
-                Rankine.value = (whatKey * 9/5) + 491.67;
-                Romer.value = (whatKey * 21/40) + 7.5;
-                break;
-            case 'Kelvin':
-                Celsius.value = whatKey - 273.15;
-                Fahrenheit.value = (whatKey - 273.15) * 9/5 + 32;
-                Rankine.value = (whatKey * 9/5);
-                Romer.value = (whatKey - 273.15) * 21/40 + 7.5;
-                break;
-            case 'Fahrenheit':
-                Celsius.value = (whatKey - 32) * 5/9;
-                Kelvin.value = (whatKey - 32) * 5/9 + 273.15;
-                Rankine.value = whatKey + 459.67;
-                Romer.value = (whatKey - 32) * 7/24 + 7.5;
-                break;
-            case 'Rankine':
-                Celsius.value = (whatKey - 491.67) * 5/9;
-                Kelvin.value = whatKey * 5/9;
-                Fahrenheit.value = whatKey - 459.67;
-                Romer.value = (whatKey - 491.67) * 7/24 + 7.5;
-                break;
-            case 'Romer':
-                Celsius.value = (whatKey - 7.5) * 40/21;
-                Kelvin.value = (whatKey - 7.5) * 40/21 + 273.15;
-                Fahrenheit.value = (whatKey - 7.5) * 24/7 + 32;
-                Rankine.value = (whatKey - 7.5) * 24/7 + 491.67;
-                break;
-        }
+            switch (targetId) {
+                case 'Celsius':
+                    Kelvin.value = whatKey + 273.15;
+                    Fahrenheit.value = (whatKey * 9 / 5) + 32;
+                    Rankine.value = (whatKey * 9 / 5) + 491.67;
+                    Romer.value = (whatKey * 21 / 40) + 7.5;
+                    break;
+                case 'Kelvin':
+                    Celsius.value = whatKey - 273.15;
+                    Fahrenheit.value = (whatKey - 273.15) * 9 / 5 + 32;
+                    Rankine.value = (whatKey * 9 / 5);
+                    Romer.value = (whatKey - 273.15) * 21 / 40 + 7.5;
+                    break;
+                case 'Fahrenheit':
+                    Celsius.value = (whatKey - 32) * 5 / 9;
+                    Kelvin.value = (whatKey - 32) * 5 / 9 + 273.15;
+                    Rankine.value = whatKey + 459.67;
+                    Romer.value = (whatKey - 32) * 7 / 24 + 7.5;
+                    break;
+                case 'Rankine':
+                    Celsius.value = (whatKey - 491.67) * 5 / 9;
+                    Kelvin.value = whatKey * 5 / 9;
+                    Fahrenheit.value = whatKey - 459.67;
+                    Romer.value = (whatKey - 491.67) * 7 / 24 + 7.5;
+                    break;
+                case 'Romer':
+                    Celsius.value = (whatKey - 7.5) * 40 / 21;
+                    Kelvin.value = (whatKey - 7.5) * 40 / 21 + 273.15;
+                    Fahrenheit.value = (whatKey - 7.5) * 24 / 7 + 32;
+                    Rankine.value = (whatKey - 7.5) * 24 / 7 + 491.67;
+                    break;
+                case 'Meter':
+                    Kmeter.value = whatKey / 1000;
+                    break;
+                case 'Kmeter':
+                    Meter.value = whatKey * 1000;
+                    break;
+            }//yep length part did not work.probably need to refresh targetId
     }
 });
 //i need to banish a few things and start some reduced test case guh
@@ -101,28 +126,19 @@ function TemperatureConvThing(){
         +   '<input type="number" id="Romer" name="Romer" step="0.01" min="-135.9" />'
         +'</div>'
 };
-//i think its working
-/*RTC (reduced test case) stuff here*/
-let RTC = Boolean();
-RTC = true;
-let test = 0;
-function RTCReplace1(){
-    return'<div id="RTC">HEY'
-    +    '<button type="button" id="RCTInvoke" onclick="RTCFunc()">RTC invoked</button>'
-    +'</div>'
-};
-function RTCReplace2(){
-    return'<div id="RTC">TWO'
-    +    '<button type="button" id="RTCInvoke" onclick="RTCFunc();">RTC invoked</button>'
-    +'</div>'
+function LengthConvThing() {
+    return '<div id="innerContent">'
+        + '<label for="Meter">m(Meter)</label>'
+        + '<label for="Kmeter">km(Kilometer)</label>'
+        + '<br>'
+        + '<input type="number" id="Meter" name="Meter" step="0.01" min="0" />'
+        + '<input type="number" id="Kmeter name="Kmeter" step="0.01" min="0"/> '
+    + '</div>'
 }
-document.getElementById("c").addEventListener("click", RTCFunc);
-function RTCFunc(){
-    if(test == 0){
-        document.getElementById("RTC").innerHTML = RTCReplace1();
-    }else{
-        document.getElementById("RTC").innerHTML = RTCReplace2();
-    }
-};//i have no idea why isn't it working - it's RTC boolean fault - actually no the if thing don't want to work with my button i even use addEventListener and it will not work still - turns out pause on exception is the problem
-console.log("I can't use replace with a button that is included in the replace?");
-
+function PlaceholderForConv() {
+    return '<div id="innerContent">'
+        + '<label for="placeholder">placeholder</label>'
+        + '<input type="number" id="placeholder" name="placeholder"/> '
+        + '</div>'
+}
+//i think its working
