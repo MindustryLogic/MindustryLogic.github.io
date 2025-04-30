6e14573d56404c1bd10db740aef02dbfd8794e4d
MindustryLogic <125643016+MindustryLogic@users.noreply.github.com>
2025-04-30 03:24:25 +0000

    numbers

--- Py/tools/math/mathPhy.js
+++ Py/tools/math/mathPhy.js
@@ -1,1 +1,13 @@
-export const G = 6.6743015e-11 //in m^3 kg^-1 s^-2, gravitational constant
+export const G = 6.6743015e-11; //in m^3 kg^-1 s^-2, gravitational constant
+export const c = 299792458; //in m s^-1, speed of light
+export const e = 2.71828; //Euler's number, approximation
+export const pi = 3.1415926535897932384626433; //Pi, approximation
+export const phi = (1 + Math.sqrt(5)) / 2; //Golden ratio, approx.
+export const commonSubstance = ["Water", "Air", "Iron", "Copper", "Aluminum"];
+//All assume at 25 C, 1 atm and 0 relative humidity until user specifies otherwise
+//Also assume all are in ideal condition (no heat lost to surroundings, wire have no resistance, etc.)
+export const commonDensity = []
+/*export var eExpand = 1;
+for (var i = 1; i < 100; i++){
+    eExpand = eExpand + 1 / Math.factorial(i);
+}*/
